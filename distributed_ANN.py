b03f2927febb3cd8d510b4a8ae2fa289d45159af
Jessa <jkarlberg@gmail.com>
2012-09-25 17:50:06 -0600

    update

--- distributed_ANN.py
+++ distributed_ANN.py
@@ -1,6 +1,7 @@
 from mpi4py import *
-import sys
-#from data_lib import DataParser
+import sys, random
+from data_lib import DataParser
+from ANN import ANN
 
 def main():
     ''' starts the distributed ANN
@@ -13,52 +14,54 @@ def main():
 
     # root node gets data from file, breaks it up, and sends out any relevant ANN information to all nodes
     if rank == 0:
-	in_layer = sys.argv[1]
-	hidden_layers = [sys.argv[2]]
+	in_layer = int(sys.argv[1])
+	hidden_layers = [int(sys.argv[2])]
 	
 	if len(sys.argv) == 6:
-		hidden_layers.append(sys.argv[3])
-		out_layer = sys.argv[4]
+		hidden_layers.append(int(sys.argv[3]))
+		out_layer = int(sys.argv[4])
 		fileName = sys.argv[5]
 	else:
-		out_layer = sys.argv[3]
+		out_layer = int(sys.argv[3])
 		fileName = sys.argv[4]
 
-
-        # get file
-        '''
         # break up data (for each node and into testing/training)
+	f = open(fileName)
         parser = DataParser(f)
         training_data = parser.get_training_sets(numNodes)
         test_data = parser.get_test_set()
         f.close()
-	'''
 
         # broadcast ANN architecture to all nodes
         comm.bcast((in_layer, hidden_layers, out_layer, fileName), root=0)
+	
     else:
         # receive ANN architecture from root
         in_layer, hidden_layers, out_layer, fileName = comm.bcast(None,root=0)
-	print hidden_layers
+	training_data = None
     
     # send out/receive training data to all nodes 
-    my_input = comm.scatter([(1,2), 2, 3, 4, 5, 6], root=0)
+    my_training_data, my_testing_data = comm.scatter(training_data, root=0)
     
     # create ANN on each node
-    ann = rank
-    '''ann = ANN(in_neurons, hidden_neurons, out_neurons)
+    ann = ANN(in_layer, hidden_layers, out_layer)
 
-    # run data through ANN
-    for i in range(0, len(my_input), 1):
-        print ann.train(my_input[i])'''
+    # train ANN
+    random.shuffle(my_training_data)
+    
+    for i in range(0, len(my_training_data), 1):
+        ann.train(my_training_data[i])
+
+    # test ANN
     
     # gather results back to root node
-    results = comm.gather(my_input, 0) # change to capital G if passing non-python class
+    results = comm.gather(ann, root=0)
     
-    if rank ==0:
+    if rank == 0:
         # combine results
         # test results
-        print results
+        print len(results)
+
 
 if __name__ == '__main__':
     main()
